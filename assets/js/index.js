2df951bf908276d8fcbc0d2e82824afd10f3476d
unknown <sajedislam019@gmail.com>
2025-02-26 22:15:56 +0600

    file added

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Sticky header
   window.onscroll = function () {
     toggleStickyHeader();
+    scrollFunction();
   };
 
   function toggleStickyHeader() {
@@ -35,6 +36,29 @@ document.addEventListener("DOMContentLoaded", function () {
       header.classList.add("sticky");
     } else {
       header.classList.remove("sticky");
+    }
+  }
+
+  // Preloader
+  window.onload = function () {
+    document.getElementById("preloader").style.display = "none";
+  };
+
+  // Scroll to Top Button
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
     }
   }
 });
